fcb7db955a577841f05415147a7d40570cb07263
ssstarss <s.s.star@mail.ru>
2024-01-19 16:36:52 +0700

    fix: clear temporary files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,13 +5,14 @@ const path = require('path');
 const srcFolder = path.join(__dirname,'files');
 const distFolder = path.join(__dirname,'files-copy');
 
-copyDir(srcFolder, distFolder);
+fsPromises.rm(distFolder, { recursive: true, force: true }).then(()=> {
+  copyDir(srcFolder, distFolder);
+});
 
 function copyDir(oldDir, newDir) {
   fsPromises.mkdir(newDir, { recursive: true }, (err) => {
     if (err) throw err;
   });
-
   fsPromises.readdir(oldDir, { withFileTypes: true }).then((fileList) => {
     fileList.forEach((file) => {
       if (file.isDirectory())
